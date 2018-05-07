21690f8c75fd27b9f2ea7155ed8147991f8bad00
dunizb <dunizb@outlook.com>
2018-05-07 17:29:07 +0800

    添加数据2

--- dist/src.997a2c11.js
+++ dist/src.997a2c11.js
@@ -10117,8 +10117,62 @@ exports.default = [{
 }, {
   id: "5",
   title: "广西·北海",
-  url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/guiling/gl27.jpg",
-  list: []
+  url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx8.jpeg",
+  list: [{
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx2.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx3.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx5.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx6.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx7.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx8.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx10.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx11.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx12.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx13.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/gx14.jpeg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai01.jpg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai02.jpg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai03.jpg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai04.jpg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai05.jpg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai07.jpg"
+  }, {
+    title: "北海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai09.jpg"
+  }]
 }];
 },{}],12:[function(require,module,exports) {
 var Vue // late bind
