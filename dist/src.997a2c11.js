85c62c341a6b34cb4b6c13477bef22752576e5d0
dunizb <dunizb@outlook.com>
2018-05-07 16:43:08 +0800

    添加数据2

--- dist/src.997a2c11.js
+++ dist/src.997a2c11.js
@@ -9959,11 +9959,83 @@ exports.default = [{
   }]
 }, {
   id: "3",
-  title: "泸沽湖",
-  url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/0.jpeg",
+  title: "丽江·泸沽湖",
+  url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/3.jpeg",
   list: [{
     title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/3.jpeg"
+  }, {
+    title: "泸沽湖",
     url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/0.jpeg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/1.jpeg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/2.jpeg"
+  }, {
+    title: "泸沽湖·草海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/4.jpeg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/5.jpeg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu5.jpeg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu6.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu7.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu8.jpg"
+  }, {
+    title: "泸沽湖·千年草甸",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/6.jpeg"
+  }, {
+    title: "泸沽湖清澈见底的水",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/7.jpeg"
+  }, {
+    title: "当地小数民族篝火晚会",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu3.jpeg"
+  }, {
+    title: "当地小数民族篝火晚会",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu4.jpg"
+  }, {
+    title: "泸沽湖·千年草海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu9.jpg"
+  }, {
+    title: "去泸沽湖的路上",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu10.jpg"
+  }, {
+    title: "泸沽湖·千年草海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu11.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu12.jpg"
+  }, {
+    title: "当地小数民族篝火晚会",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu13.jpg"
+  }, {
+    title: "泸沽湖·千年草海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu14.jpg"
+  }, {
+    title: "泸沽湖·千年草海",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu15.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu16.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu17.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu18.jpg"
+  }, {
+    title: "泸沽湖",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/star1/luguhu/luguhu19.jpg"
   }]
 }];
 },{}],12:[function(require,module,exports) {
