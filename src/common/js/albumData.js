de4d14ae665af82e30ab01fdc81c0a07f083a3c4
dunizb <dunizb@outlook.com>
2018-05-07 18:29:39 +0800

    添加数据

--- src/common/js/albumData.js
+++ src/common/js/albumData.js
@@ -453,5 +453,128 @@ export default [
         url: "https://dunizb.b0.upaiyun.com/demo/photo/gx/beihai/beihai09.jpg"
       },
     ]
+  },
+  {
+    id:"6",
+    title: "北京·世界公园·长城·天堂·颐和园",
+    url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj01.jpg",
+    list: [
+      {
+        title: "北海公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj01.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj02.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj03.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj04.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj06.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj07.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj08.jpg"
+      },
+      {
+        title: "世界公园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/bj09.jpg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng2.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng3.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng4.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng5.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng6.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng7.jpeg"
+      },
+      {
+        title: "八达岭长城",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/changcheng8.jpeg"
+      },
+      {
+        title: "故宫",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/gugong.jpeg"
+      },
+      {
+        title: "故宫",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/gugong2.jpeg"
+      },
+      {
+        title: "故宫",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/gugong3.jpeg"
+      },
+      {
+        title: "天安门",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/tiananmen.jpeg"
+      },
+      {
+        title: "天安门",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/tiananmen1.jpeg"
+      },
+      {
+        title: "天安门广场",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/tiananmenguangchang.jpeg"
+      },
+      {
+        title: "天堂",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/tiantang.jpeg"
+      },
+      {
+        title: "天堂",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/tiantang2.jpeg"
+      },
+      {
+        title: "颐和园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/yiheyuan.jpeg"
+      },
+      {
+        title: "颐和园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/yiheyuan2.jpeg"
+      },
+      {
+        title: "颐和园",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/yiheyuan3.jpeg"
+      },
+      {
+        title: "2014年的我",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/tj.jpeg"
+      },
+      {
+        title: "中南海大门",
+        url: "https://dunizb.b0.upaiyun.com/demo/photo/beijing/znh.webp"
+      },
+    ]
   }
 ]
